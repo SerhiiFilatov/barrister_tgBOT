9bb0bf358e6ca7af4c16b98ecea6727342104b4e
Serhii <fsn9792@gmail.com>
2025-08-13 13:55:42 +0300

    webhook

--- main.py
+++ main.py
@@ -3,61 +3,6 @@ import asyncpg
 import os
 from aiohttp import web
 from dotenv import load_dotenv
-
-
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram_dialog import setup_dialogs
-from aiogram.client.default import DefaultBotProperties
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-
-# from config import settings
-from bot_dialogs import main_menu, faq, ask_a_lawyer, book_a_consultation
-from bot_handlers import start_exit_menu, get_information_and_stat, admin_menu
-from bot_middlewares.reg_middleware import DbSessionMiddleware
-
-load_dotenv()
-
-# async def create_pool():
-#     return await asyncpg.create_pool(user=os.getenv('DB_USER'),
-#                                      password=os.getenv('DB_PASSWORD'),
-#                                      host=os.getenv('DB_HOST'),
-#                                      port=os.getenv('DB_PORT'),
-#                                      database=os.getenv('DATABASE'))
-# #
-# async def main() -> None:
-#     bot = Bot(
-#         token=os.getenv('BOT_TOKEN'),
-#         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
-#     )
-#     dp = Dispatcher(storage=MemoryStorage())
-#
-#     dp.include_router(admin_menu.router)
-#     dp.include_router(start_exit_menu.router)
-#     dp.include_router(get_information_and_stat.router)
-#
-#     dp.include_router(book_a_consultation.appointment_for_a_consultation_dialog)
-#     dp.include_router(main_menu.main_menu_dialog)
-#     dp.include_router(ask_a_lawyer.ask_lawyer_dialog)
-#     dp.include_router(faq.faq_dialog)
-#
-#     setup_dialogs(dp)
-#     pool_connect = await create_pool()
-#     dp.update.middleware.register(DbSessionMiddleware(pool_connect))
-#
-#     await bot.delete_webhook(drop_pending_updates=True)
-#     await dp.start_polling(bot)
-#
-#
-# if __name__ == '__main__':
-#     asyncio.run(main())
-
-import asyncio
-import asyncpg
-import os
-from aiohttp import web
-from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 from aiogram import Bot, Dispatcher
