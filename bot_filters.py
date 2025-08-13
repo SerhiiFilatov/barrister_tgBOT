3582304a5d019dce85dab57b1be9c409120cbe1d
Serhii <fsn9792@gmail.com>
2025-08-13 17:56:09 +0300

    filter id

--- bot_filters.py
+++ bot_filters.py
@@ -7,5 +7,8 @@ from aiogram.types import Message
 load_dotenv()
 
 class AdminFilter(BaseFilter):
+    def __init__(self):
+        self.admin_id = int(os.getenv('LAWYER_ID'))
+
     async def __call__(self, message: Message) -> bool:
-        return message.from_user.id == os.getenv('LAWYER_ID')
+        return message.from_user.id == self.admin_id
